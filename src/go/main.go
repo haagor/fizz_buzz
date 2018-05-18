6847b528ca89ab2f2b4dd3de59896fcfc70f410d
haag <sim.paris@wanadoo.fr>
2018-05-18 23:34:04 +0200

    hello POST Json

--- src/go/main.go
+++ src/go/main.go
@@ -2,19 +2,37 @@ package main
 
 import (
     "fmt"
+    "io/ioutil"
+    "encoding/json"
     "log"
     "net/http"
 
     "github.com/gorilla/mux"
 )
 
-// our main function
+
 func main() {
     l_router := mux.NewRouter()
-    l_router.HandleFunc("/hello", hello).Methods("GET")
+    l_router.HandleFunc("/hello", hello).Methods("POST")
+    fmt.Println("listening...")
     log.Fatal(http.ListenAndServe(":8000", l_router))
 }
 
+
+type Data struct {
+    Message string
+}
+
 func hello(p_writer http.ResponseWriter, p_request *http.Request) {
-	fmt.Fprintln(p_writer, "hello you!")
+    l_data_receive,_ := ioutil.ReadAll(p_request.Body)
+    var l_data_json Data
+    json.Unmarshal(l_data_receive, &l_data_json)
+
+    var l_response Data
+    if l_data_json.Message == "hello" {
+        l_response.Message = "hello you !"
+    } else {
+        l_response.Message = "hello?"
+    }
+    json.NewEncoder(p_writer).Encode(l_response)
 }
